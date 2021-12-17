2ecbe90ac9abb130bed9de07e7e569b83ae1a5a6
Aleksandr Garkovenko <agarkovenko@tradingview.com>
2021-12-17 20:14:28 +0300

    close

--- server/message.go
+++ server/message.go
@@ -10,6 +10,6 @@ type RegisterMSG struct {
 	X         int    `json:"x"`
 	Y         int    `json:"y"`
 	Method    string `json:"method"`
-	SessionId int    `json:sessionId`
-	RoomId    int    `json:RoomId`
+	SessionId int    `json:"sessionId"`
+	RoomId    int    `json:"roomId"`
 }
